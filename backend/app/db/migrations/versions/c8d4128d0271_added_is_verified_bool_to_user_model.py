c69a5a2b79ed3bbd72e3e02241cbd21f5f5d2f45
joey <jotosound@gmail.com>
2023-06-02 22:12:52 -0400

    Added is_verified to User model. Created migration. Updated /items route

---
+++ backend/app/db/migrations/versions/c8d4128d0271_added_is_verified_bool_to_user_model.py
@@ -0,0 +1,24 @@
+"""Added is_verified bool to User Model
+
+Revision ID: c8d4128d0271
+Revises: fdf8821871d7
+Create Date: 2023-06-03 01:43:29.263372
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+revision = 'c8d4128d0271'
+down_revision = 'fdf8821871d7'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.add_column('users', sa.Column('is_verified', sa.Boolean(), nullable=True))
+
+
+def downgrade() -> None:
+    op.drop_column('users', 'is_verified')
+
