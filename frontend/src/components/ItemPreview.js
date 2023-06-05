0e60beebc5f85780fd1e93f616ecaae1b6dc1bbf
joey <jotosound@gmail.com>
2023-06-04 22:27:39 -0400

    added svg and TOP SELLER text to ItemPreview

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -54,6 +54,7 @@ const ItemPreview = (props) => {
               className="user-pic rounded-circle pr-1"
             />
           </Link>
+          {item.seller.isVerified === true ? <><img src="/verified_seller.svg" alt="verified seller"/> <span>TOP SELLER</span></>: null}
           <button className="btn btn-outline-secondary" onClick={handleClick}>
             <i className="ion-heart"></i> {item.favoritesCount}
           </button>
